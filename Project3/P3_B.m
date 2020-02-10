f3fe087dc5f6143b910ee5b1d1f56747d3e18ecf
praisejimmy <ryan@ryanmyers.com>
2020-02-09 21:32:20 -0800

    Project 4 made

--- Project3/P3_B.m
+++ Project3/P3_B.m
@@ -2,64 +2,64 @@ clc
 close all
 
 correct = 0;
+% 
+% im = imread('rock_far.jpg');
+% result = rps(im);
+% fprintf('\nExpected answer: rock');
+% fprintf('\nanswer: %s\n', result);
+% if strcmp(result, 'rock')
+%     correct = correct + 1;
+% end 
+% 
+% im = imread('rock_close.jpg');
+% result = rps(im);
+% fprintf('\nExpected answer: rock');
+% fprintf('\nanswer: %s\n', result);
+% if strcmp(result, 'rock')
+%     correct = correct + 1;
+% end 
+% 
+% im = imread('scissors_medium.jpg');
+% result = rps(im);
+% fprintf('\nExpected answer: scissors');
+% fprintf('\nanswer: %s\n', result);
+% if strcmp(result, 'scissors')
+%     correct = correct + 1;
+% end 
+% 
+% im = imread('paper_medium.jpg');
+% result = rps(im);
+% fprintf('\nExpected answer: paper');
+% fprintf('\nanswer: %s\n', result);
+% if strcmp(result, 'paper')
+%     correct = correct + 1;
+% end 
+% 
+% im = imread('scissors_close.jpg');
+% result = rps(im);
+% fprintf('\nExpected answer: scissors');
+% fprintf('\nanswer: %s\n', result);
+% if strcmp(result, 'scissors')
+%     correct = correct + 1;
+% end 
+% 
+% im = imread('paper_close.jpg');
+% result = rps(im);
+% fprintf('\nExpected answer: paper');
+% fprintf('\nanswer: %s\n', result);
+% if strcmp(result, 'paper')
+%     correct = correct + 1;
+% end 
+% 
+% im = imread('rock_medium.jpg');
+% result = rps(im);
+% fprintf('\nExpected answer: rock');
+% fprintf('\nanswer: %s\n', result);
+% if strcmp(result, 'rock')
+%     correct = correct + 1;
+% end 
 
-im = imread('rock_far.jpg');
-result = rps(im);
-fprintf('\nExpected answer: rock');
-fprintf('\nanswer: %s\n', result);
-if strcmp(result, 'rock')
-    correct = correct + 1;
-end 
-
-im = imread('rock_close.jpg');
-result = rps(im);
-fprintf('\nExpected answer: rock');
-fprintf('\nanswer: %s\n', result);
-if strcmp(result, 'rock')
-    correct = correct + 1;
-end 
-
-im = imread('scissors_medium.jpg');
-result = rps(im);
-fprintf('\nExpected answer: scissors');
-fprintf('\nanswer: %s\n', result);
-if strcmp(result, 'scissors')
-    correct = correct + 1;
-end 
-
-im = imread('paper_medium.jpg');
-result = rps(im);
-fprintf('\nExpected answer: paper');
-fprintf('\nanswer: %s\n', result);
-if strcmp(result, 'paper')
-    correct = correct + 1;
-end 
-
-im = imread('scissors_close.jpg');
-result = rps(im);
-fprintf('\nExpected answer: scissors');
-fprintf('\nanswer: %s\n', result);
-if strcmp(result, 'scissors')
-    correct = correct + 1;
-end 
-
-im = imread('paper_close.jpg');
-result = rps(im);
-fprintf('\nExpected answer: paper');
-fprintf('\nanswer: %s\n', result);
-if strcmp(result, 'paper')
-    correct = correct + 1;
-end 
-
-im = imread('rock_medium.jpg');
-result = rps(im);
-fprintf('\nExpected answer: rock');
-fprintf('\nanswer: %s\n', result);
-if strcmp(result, 'rock')
-    correct = correct + 1;
-end 
-
-im = imread('scissors_medium.jpg');
+im = imread('scissors_far.jpg');
 result = rps(im);
 fprintf('\nExpected answer: scissors');
 fprintf('\nanswer: %s\n', result);
